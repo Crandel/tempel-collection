9a536f2013d46379adb17ba06d7b4230e9a34d0b
Crandel <4005032+Crandel@users.noreply.github.com>
2022-11-22 22:29:28 +0100

    Melpa compatible LICENSE

--- tempel-collections.el
+++ tempel-collections.el
@@ -1,5 +1,28 @@
 ;;; tempel-collections.el --- Collection templates for tempel
 
+;; Copyright (C) 2022 Vitalii Drevenchuk
+
+;; Author: Vitalii Drevenchuk <cradlemann@gmail.com>
+;; Keywords: tempel
+;; Version: 1.0.0
+;; Package-Requires: ((tempel "20221016.1017"))
+;; Homepage: https://github.com/Crandel/tempel-collections
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;; This package adds support for templates using tempel.  It will add templates from
 ;; template directory to tempel-path.
